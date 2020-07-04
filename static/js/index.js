1b6e01fbaced33a41ab25620043b001567d137ae
rajvir.jhawar@gmail.com <rajvir.jhawar@gmail.com>
2020-07-04 17:18:19 -0600

    Npm package updates and changed js loc

--- static/js/index.js
+++ static/js/index.js
@@ -6,7 +6,7 @@ import 'babylon-vrm-loader'
 'use strict';
 const canvas = document.getElementById("renderCanvas"); // Get the canvas element
 const engine = new BABYLON.Engine(canvas, true); // Generate the BABYLON 3D engine
-let charList = ["e.vrm"];//, "b.vrm", "c.vrm", "d.vrm"];
+let charList = ["a.vrm"];//, "b.vrm", "c.vrm", "d.vrm"];
 let divFps = document.getElementById("fps");
 
 
